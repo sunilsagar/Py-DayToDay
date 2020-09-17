e9adda143d3af255598c45427c106279b3c7d953
ndas1971 <tradepksfounder@gmail.com>
2020-09-17 14:20:51 +0530

    DAY4

--- mex_test.py
+++ mex_test.py
@@ -2,3 +2,5 @@ import mex
 
 result = mex.square(10)
 print(result)
+lst = [1,2,3]
+print("Mean=", mex.mean(lst), "SD=", mex.sd(lst) )
---
+++ test_class.py
@@ -0,0 +1,7 @@
+from classes import MyInt
+
+a = MyInt(2) #1, 2-> v , a-> self
+b = MyInt(3) #1
+c = a + b #2, b-> other, a -> self 
+print(a.square())
+print(a) #3
